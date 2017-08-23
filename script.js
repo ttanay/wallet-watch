98ef9420362c482aa076286fe88bc159c1356b30
ttanay <ttanay100@gmail.com>
2017-08-23 22:15:20 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.modal').modal({
+        dismissable: true
+    });
+    $('.edit-action').click(function(){
+        $('#edit').modal('open');
+    });
+});
+
+function grabExpenseData(calledExpense){
+    
+}
